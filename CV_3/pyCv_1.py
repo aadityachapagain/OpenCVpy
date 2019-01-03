7549a76176fa671f50679f4e8c80ed92ba21edae
Aaditya Chapagain <chapagain.aaditya@gmail.com>
2019-01-03 11:37:18 +0545

    affine wrapping try

---
+++ CV_3/pyCv_1.py
@@ -0,0 +1,12 @@
+from scipy import ndimage
+from PIL import Image
+from pylab import *
+
+im = array(Image.open('../data/rome.jpg').convert('L'))
+H = array([[1.4,0.05,-100],[0.05,1.5,-100],[0,0,1]])
+im2 = ndimage.affine_transform(im,H[:2,:2],(H[0,2],H[1,2]))
+
+figure()
+gray()
+imshow(im2)
+show()
